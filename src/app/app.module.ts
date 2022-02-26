f92cbbc71b4224bb7f0dd27d8b8e3ceb268fb500
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-02-26 14:25:24 +0100

    -added: twitter module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from 'src/database/database.module';
+import { TwitterModule } from 'src/twitter/twitter.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    TwitterModule,
+  ],
 })
 export class AppModule {}
