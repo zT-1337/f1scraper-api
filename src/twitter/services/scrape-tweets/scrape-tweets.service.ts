2f0b8194be84eb62e502555bc19b11ebf3dafea4
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-02-26 16:09:17 +0100

    -added: scrape service to providers

--- src/twitter/services/scrape-tweets/scrape-tweets.service.ts
+++ src/twitter/services/scrape-tweets/scrape-tweets.service.ts
@@ -1,11 +1,31 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { ScraperSource } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
+import { TwitterScraperConfig } from 'src/twitter/configs/twitter-scraper-config';
 
 @Injectable()
 export class ScrapeTweetsService {
-  constructor(@Inject(DatabaseService) private dbService: DatabaseService) {}
+  private readonly logger = new Logger(ScrapeTweetsService.name);
 
+  constructor(
+    @Inject(DatabaseService) private dbService: DatabaseService,
+    @Inject(TwitterScraperConfig) private config: TwitterScraperConfig,
+  ) {}
+
+  @Cron(CronExpression.EVERY_10_SECONDS)
   public async scrapeNewTweets(): Promise<void> {
+    const lastTwitterLookup = await this.dbService.scraperLookup.findUnique({
+      where: {
+        source: ScraperSource.TWITTER,
+      },
+    });
+
+    if (lastTwitterLookup === null) {
+      this.logger.error('Twitter Scrape Source is missing in DB');
+      return;
+    }
+
     return;
   }
 }
--- src/twitter/twitter.module.ts
+++ src/twitter/twitter.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DatabaseModule } from 'src/database/database.module';
 import { TwitterClient } from './clients/twitter-client';
 import {
   TwitterClientConfig,
   twitterClientConfigFactory,
 } from './configs/twitter-client-config';
+import {
+  TwitterScraperConfig,
+  twitterScraperConfigFactory,
+} from './configs/twitter-scraper-config';
+import { ScrapeTweetsService } from './services/scrape-tweets/scrape-tweets.service';
 
 @Module({
   providers: [
@@ -14,6 +20,13 @@ import {
       useFactory: twitterClientConfigFactory,
       inject: [ConfigService],
     },
+    ScrapeTweetsService,
+    {
+      provide: TwitterScraperConfig,
+      useFactory: twitterScraperConfigFactory,
+      inject: [ConfigService],
+    },
   ],
+  imports: [DatabaseModule],
 })
 export class TwitterModule {}
