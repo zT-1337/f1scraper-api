c720e596834138c9ca54d7eaa60e872ee0ad4d2f
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-02-26 13:17:49 +0100

    -added: dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { config as envConfig } from 'dotenv';
+
+envConfig();
 
 async function bootstrap() {
+  const port = parseInt(process.env.PORT) || 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
+
 bootstrap();
