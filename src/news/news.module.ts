3f2f21cd51ae2c2ed702f4c24dead5eac6383a17
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-02-26 18:20:46 +0100

    -added: NewsController

--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
+import { NewsController } from './controllers/news/news.controller';
 import { GetNewsService } from './services/get-news/get-news.service';
 
 @Module({
   providers: [GetNewsService],
   imports: [DatabaseModule],
+  controllers: [NewsController],
 })
 export class NewsModule {}
