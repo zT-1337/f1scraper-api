324c6223bce22c1e02757bce299a86cd538e0800
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-03-02 17:10:41 +0100

    -changed: cron expression

--- src/twitter/services/scrape-tweets/scrape-tweets.service.ts
+++ src/twitter/services/scrape-tweets/scrape-tweets.service.ts
@@ -84,7 +84,7 @@ export class ScrapeTweetsService {
     return;
   }
 
-  @Cron(CronExpression.EVERY_HOUR)
+  @Cron(CronExpression.EVERY_30_SECONDS)
   public async scrapeJob() {
     //This locking mechanism only works because of the single threadedness of node.js
     if (this.isScraping) return;
